9b0fc8af563483e23b758b6dbe0f045ebcffdb03
Christian-Michel Joseph <94189803+chrimijo@users.noreply.github.com>
2024-03-07 13:54:35 +0100

    merging

---
+++ modules/deploy-openai-model-2-apim/variables.tf
@@ -0,0 +1,83 @@
+####################################
+######### Define variables #########
+####################################
+
+variable "AZURE_CD_PREFIX" {
+  type    = string
+ validation {
+    condition     = length(var.AZURE_CD_PREFIX) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+variable "SUFFIX" {
+  type    = string
+ validation {
+    condition     = length(var.SUFFIX) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+      variable "COGNITIVEACID" {
+  type    = string
+ validation {
+    condition     = length(var.COGNITIVEACID) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+
+  variable "GPTMODEL" {
+  type    = string
+ validation {
+    condition     = length(var.GPTMODEL) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+    variable "GPTVERSION" {
+  type    = string
+ validation {
+    condition     = length(var.GPTVERSION) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+      variable "AZURE_API_PREFIX" {
+  type    = string
+ validation {
+    condition     = length(var.AZURE_API_PREFIX) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+    variable "RGNAME" {
+  type    = string
+ validation {
+    condition     = length(var.RGNAME) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+
+     variable "APIMNGNAME" {
+  type    = string
+ validation {
+    condition     = length(var.APIMNGNAME) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+ 
+
+
+      variable "AZURE_APIM_PREFIX" {
+  type    = string
+ validation {
+    condition     = length(var.AZURE_APIM_PREFIX) > 0
+    error_message = "Cannot be empty"
+  }  
+  }
+
+
+
+
