4aaca866239bbe60c89cebf16c269500fb8df68a
Christian-Michel Joseph <94189803+chrimijo@users.noreply.github.com>
2024-03-08 18:00:10 +0100

    update

---
+++ modules/deploy-openaimodel-and-api/api-openai.tf
@@ -0,0 +1,89 @@
+# Deploy OpenAI model based on child'module input parameters
+resource "azurerm_cognitive_deployment" "model" {
+ name                 = "${var.AZURE_CD_PREFIX}-openai-${var.SUFFIX}"
+ cognitive_account_id = "${var.COGNITIVEACID}"
+  model {
+    format  = "OpenAI"
+    name    = "${var.GPTMODEL}"
+    version = "${var.GPTVERSION}"
+  }
+  scale {
+    type = "Standard"
+  }
+}
+
+# Deploy API to the model using an XML files get on 
+resource "azurerm_api_management_api" "api-model" {
+  name                  = "${var.AZURE_API_PREFIX}-openai-${trim(var.GPTMODEL,"-")}-${trim(var.GPTVERSION,"-")}"
+  resource_group_name   = var.RGNAME
+  api_management_name   = var.APIMNGNAME
+  revision              = "1"
+  display_name          = "${var.AZURE_API_PREFIX}-openai-${trim(var.GPTMODEL,"-")}-${trim(var.GPTVERSION,"-")}"
+  protocols             = ["https"]
+  #service_url           = "https://openai-daei-di01-rec-openai-frct.openai.azure.com/openai"
+  service_url = "${var.OPENAI_URL}openai"
+  subscription_required = true
+
+  subscription_key_parameter_names {
+    header = "Api-Key"
+    query  = "subscription-key"
+  }
+
+  import {
+    content_format = "openapi"
+    content_value  = file("./api-definitions-files/AzureOpenAI-2023-03-15-preview-inference.json")
+  }
+
+  depends_on = [azurerm_cognitive_deployment.model]
+}
+
+resource "azurerm_api_management_product" "openai-product" {
+  product_id            = "openai-product"
+  resource_group_name   = var.RGNAME
+  api_management_name   = var.APIMNGNAME
+
+  display_name          = "openai-product"
+  description           = "openai-product"
+  subscription_required = true
+  approval_required     = false
+  published             = true
+}
+
+resource "azurerm_api_management_product_api" "openai-product-api" {
+  api_name            = azurerm_api_management_api.api-model.name
+  product_id          = azurerm_api_management_product.openai-product.product_id
+  api_management_name = var.APIMNGNAME
+  resource_group_name = var.RGNAME
+}
+
+resource "azurerm_api_management_subscription" "openai-subscription" {
+  resource_group_name = var.RGNAME
+  api_management_name = var.APIMNGNAME
+  product_id          = azurerm_api_management_product.openai-product.id
+  display_name        = "openai-subscription-key"
+  allow_tracing = false
+}
+
+
+resource "azurerm_api_management_api_policy" "api_policy" {
+  api_name            = azurerm_api_management_api.api-model.name
+  api_management_name = var.APIMNGNAME
+  resource_group_name = var.RGNAME
+  xml_content = <<-XML
+<policies>
+    <inbound>
+        <base />
+        <authentication-managed-identity resource="https://cognitiveservices.azure.com" />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+}
